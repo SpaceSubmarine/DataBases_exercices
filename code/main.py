31b367f3ea9acc1a6051b96e3fe59f5293b24c79
Marc <marc.montalvez@gmail.com>
2021-11-28 18:10:10 +0100

    primer control de versiones

---
+++ code/main.py
@@ -0,0 +1,7 @@
+from Cities import Cities
+
+city = Cities()
+#city.elimina_ciudad(8)
+#city.inserta_ciudad('HHH','city Name','capital','MMN')
+city.modifica_ciudad(7, 'HHH','PAIN INDIA', 'CAP INDIA', 'GGG')
+print(city)
